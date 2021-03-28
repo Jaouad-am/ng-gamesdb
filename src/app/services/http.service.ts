b50dd10f2acc4edbb9d2911dd4e6770924d3e5cc
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2021-03-28 10:29:56 +0100

    typescript models

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { env } from 'process';
 import { Observable } from 'rxjs';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root'
